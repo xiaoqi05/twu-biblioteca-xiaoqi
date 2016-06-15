79123b7e76a6ccd25b1c5df81ab7491d46315821
xiaoqi05 <772508341@qq.com>
2016-06-15 17:36:01 +0800

    adjust the order of menus

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -12,9 +12,9 @@ public class BibliotecaApp {
         bookList.add(new Book("xiaoq1i", "timess", "2014-5", 2, true));
         ArrayList<Option> optionList = new ArrayList<Option>();
         optionList.add(new ListBooks("ShowAllBook", 1));
-        optionList.add(new Quit("Quit", 2));
-        optionList.add(new checkoutBook("checkoutBook", 3));
-        optionList.add(new returnBook("returnBook", 4));
+        optionList.add(new checkoutBook("checkoutBook", 2));
+        optionList.add(new returnBook("returnBook", 3));
+        optionList.add(new Quit("Quit", 4));
         Biblioteca biblioteca = new Biblioteca(bookList, optionList);
         biblioteca.start();
 
